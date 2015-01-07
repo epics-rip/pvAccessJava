8229b199704121140db8df386a02b8db3e0b45c3
Matej Sekoranja <unknown>
2015-01-07 10:01:21 +0100

    minor refactoring

---
+++ src/org/epics/pvaccess/client/pvds/test/PVDataSerializationSize.java
@@ -0,0 +1,149 @@
+package org.epics.pvaccess.client.pvds.test;
+
+import org.epics.pvdata.pv.Field;
+import org.epics.pvdata.pv.PVField;
+import org.epics.pvdata.pv.PVScalarArray;
+import org.epics.pvdata.pv.PVString;
+import org.epics.pvdata.pv.PVStringArray;
+import org.epics.pvdata.pv.PVStructure;
+import org.epics.pvdata.pv.PVStructureArray;
+import org.epics.pvdata.pv.PVUnion;
+import org.epics.pvdata.pv.PVUnionArray;
+import org.epics.pvdata.pv.Scalar;
+import org.epics.pvdata.pv.ScalarType;
+import org.epics.pvdata.pv.StringArrayData;
+import org.epics.pvdata.pv.StructureArrayData;
+import org.epics.pvdata.pv.UnionArrayData;
+
+public final class PVDataSerializationSize {
+
+	public static final int[] scalarSize = {
+		1, // pvBoolean
+		1,    // pvByte
+		2,   // pvShort
+		4,     // pvInt
+		8,    // pvLong
+		1,   // pvUByte
+		2,  // pvUShort
+		4,    // pvUInt
+		8,   // pvULong
+		4,   // pvFloat
+		8,  // pvDouble
+		0   // pvString
+	};
+
+	public static int getSerializationSize(PVField pvField)
+	{
+		Field field = pvField.getField();
+		switch (field.getType())
+		{
+		case scalar:
+		{
+			ScalarType scalarType = ((Scalar)field).getScalarType();
+			if (scalarType == ScalarType.pvString)
+				return getStringSerializationSize(((PVString)pvField).get());
+			else
+				return scalarSize[scalarType.ordinal()];
+		}
+		case structure:
+		{
+			return getStructureSerializationSize((PVStructure)pvField);
+		}
+		case union:
+		{
+			return getUnionSerializationSize((PVUnion)pvField);
+		}
+		case scalarArray:
+		{
+			PVScalarArray pvScalarArray = (PVScalarArray)pvField;
+			int arrayLength = pvScalarArray.getLength();
+			ScalarType elementType = pvScalarArray.getScalarArray().getElementType();
+			if (elementType == ScalarType.pvString)
+			{
+				StringArrayData sad = new StringArrayData();
+				((PVStringArray)pvScalarArray).get(0, arrayLength, sad);
+				int size = getSerializationSizeSize(arrayLength);
+				for (String s : sad.data)
+					size += getStringSerializationSize(s);
+				return size;
+			}
+			else
+				return getSerializationSizeSize(arrayLength) + arrayLength * scalarSize[elementType.ordinal()];
+		}
+		case structureArray:
+		{
+			PVStructureArray pvStructureArray = (PVStructureArray)pvField;
+			int arrayLength = pvStructureArray.getLength();
+			int size = getSerializationSizeSize(arrayLength);
+			StructureArrayData sad = new StructureArrayData();
+			pvStructureArray.get(0, arrayLength, sad);
+			for (PVStructure s : sad.data)
+				size += 1 + getStructureSerializationSize(s);
+			return size;
+		}
+		case unionArray:
+		{
+			PVUnionArray pvUnionArray = (PVUnionArray)pvField;
+			int arrayLength = pvUnionArray.getLength();
+			int size = getSerializationSizeSize(arrayLength);
+			UnionArrayData sad = new UnionArrayData();
+			pvUnionArray.get(0, arrayLength, sad);
+			for (PVUnion s : sad.data)
+				size += 1 + getUnionSerializationSize(s);
+			return size;
+		}
+		default:
+			throw new IllegalStateException("unsupported type " + pvField.getField());
+		}
+	}
+
+	public static int getSerializationSizeSize(int size)
+	{
+		if (size == -1)	
+			return 1;
+		else if (size < 254)
+			return 1;
+		else
+			return 5;
+	}
+
+	public static int getStringSerializationSize(String str)
+	{
+		int len = str.length();
+		return getSerializationSizeSize(len) + len;
+	}
+
+	public static int getStructureSerializationSize(PVStructure pvStructure)
+	{
+		PVField[] pvFields = pvStructure.getPVFields();
+		int size = getSerializationSizeSize(pvFields.length);
+		for (PVField pvf : pvFields)
+			size += getSerializationSize(pvf);
+		return size;
+	}
+
+	public static int getUnionSerializationSize(PVUnion pvUnion)
+	{
+		PVField value = pvUnion.get();
+		if (pvUnion.getUnion().isVariant())
+		{
+			if (value == null)
+				return 1;
+			else
+			{
+				throw new RuntimeException("not supported");
+				// TODO 
+				//return getSerializationSize(value.getField()) + getSerializationSize(value);
+			}
+		}
+		else
+		{
+			int selector = pvUnion.getSelectedIndex();
+			int size = getSerializationSizeSize(selector);
+			if (selector != PVUnion.UNDEFINED_INDEX) 
+				size += getSerializationSize(value);
+			return size;
+		}
+	}
+
+}
--- src/org/epics/pvaccess/client/pvds/test/TestPVDS.java
+++ src/org/epics/pvaccess/client/pvds/test/TestPVDS.java
@@ -39,20 +39,9 @@ import org.epics.pvdata.pv.DeserializableControl;
 import org.epics.pvdata.pv.Field;
 import org.epics.pvdata.pv.PVByteArray;
 import org.epics.pvdata.pv.PVField;
-import org.epics.pvdata.pv.PVScalarArray;
-import org.epics.pvdata.pv.PVString;
-import org.epics.pvdata.pv.PVStringArray;
-import org.epics.pvdata.pv.PVStructure;
-import org.epics.pvdata.pv.PVStructureArray;
-import org.epics.pvdata.pv.PVUnion;
-import org.epics.pvdata.pv.PVUnionArray;
-import org.epics.pvdata.pv.Scalar;
 import org.epics.pvdata.pv.ScalarType;
 import org.epics.pvdata.pv.SerializableControl;
-import org.epics.pvdata.pv.StringArrayData;
 import org.epics.pvdata.pv.Structure;
-import org.epics.pvdata.pv.StructureArrayData;
-import org.epics.pvdata.pv.UnionArrayData;
 
 public class TestPVDS {
 
@@ -1594,7 +1583,7 @@ public class TestPVDS {
 		// not thread-safe
 	    public void send(PVField data) throws InterruptedException
 	    {
-	    	int dataSize = getSerializationSize(data);
+	    	int dataSize = PVDataSerializationSize.getSerializationSize(data);
 
 	    	takeFreeBuffer();
 		    
@@ -1759,136 +1748,6 @@ public class TestPVDS {
 			add("value", ScalarType.pvInt).
 			createStructure();
 
-	public static final int[] scalarSize = {
-		1, // pvBoolean
-		1,    // pvByte
-		2,   // pvShort
-		4,     // pvInt
-		8,    // pvLong
-		1,   // pvUByte
-		2,  // pvUShort
-		4,    // pvUInt
-		8,   // pvULong
-		4,   // pvFloat
-		8,  // pvDouble
-		0   // pvString
-	};
-
-	public static int getSerializationSizeSize(int size)
-	{
-		if (size == -1)	
-			return 1;
-		else if (size < 254)
-			return 1;
-		else
-			return 5;
-	}
-	
-	public static int getStringSerializationSize(String str)
-	{
-		int len = str.length();
-		return getSerializationSizeSize(len) + len;
-	}
-	
-	public static int getStructureSerializationSize(PVStructure pvStructure)
-	{
-		PVField[] pvFields = pvStructure.getPVFields();
-		int size = getSerializationSizeSize(pvFields.length);
-		for (PVField pvf : pvFields)
-			size += getSerializationSize(pvf);
-		return size;
-	}
-	
-	public static int getUnionSerializationSize(PVUnion pvUnion)
-	{
-		PVField value = pvUnion.get();
-		if (pvUnion.getUnion().isVariant())
-		{
-			if (value == null)
-				return 1;
-			else
-			{
-				throw new RuntimeException("not supported");
-				// TODO 
-				//return getSerializationSize(value.getField()) + getSerializationSize(value);
-			}
-		}
-		else
-		{
-			int selector = pvUnion.getSelectedIndex();
-			int size = getSerializationSizeSize(selector);
-			if (selector != PVUnion.UNDEFINED_INDEX) 
-				size += getSerializationSize(value);
-			return size;
-		}
-	}
-	
-	// TODO alignment for arrays
-	public static int getSerializationSize(PVField pvField)
-	{
-		Field field = pvField.getField();
-		switch (field.getType())
-		{
-		case scalar:
-		{
-			ScalarType scalarType = ((Scalar)field).getScalarType();
-			if (scalarType == ScalarType.pvString)
-				return getStringSerializationSize(((PVString)pvField).get());
-			else
-				return scalarSize[scalarType.ordinal()];
-		}
-		case structure:
-		{
-			return getStructureSerializationSize((PVStructure)pvField);
-		}
-		case union:
-		{
-			return getUnionSerializationSize((PVUnion)pvField);
-		}
-		case scalarArray:
-		{
-			PVScalarArray pvScalarArray = (PVScalarArray)pvField;
-			int arrayLength = pvScalarArray.getLength();
-			ScalarType elementType = pvScalarArray.getScalarArray().getElementType();
-			if (elementType == ScalarType.pvString)
-			{
-				StringArrayData sad = new StringArrayData();
-				((PVStringArray)pvScalarArray).get(0, arrayLength, sad);
-				int size = getSerializationSizeSize(arrayLength);
-				for (String s : sad.data)
-					size += getStringSerializationSize(s);
-				return size;
-			}
-			else
-				return getSerializationSizeSize(arrayLength) + arrayLength * scalarSize[elementType.ordinal()];
-		}
-		case structureArray:
-		{
-			PVStructureArray pvStructureArray = (PVStructureArray)pvField;
-			int arrayLength = pvStructureArray.getLength();
-			int size = getSerializationSizeSize(arrayLength);
-			StructureArrayData sad = new StructureArrayData();
-			pvStructureArray.get(0, arrayLength, sad);
-			for (PVStructure s : sad.data)
-				size += 1 + getStructureSerializationSize(s);
-			return size;
-		}
-		case unionArray:
-		{
-			PVUnionArray pvUnionArray = (PVUnionArray)pvField;
-			int arrayLength = pvUnionArray.getLength();
-			int size = getSerializationSizeSize(arrayLength);
-			UnionArrayData sad = new UnionArrayData();
-			pvUnionArray.get(0, arrayLength, sad);
-			for (PVUnion s : sad.data)
-				size += 1 + getUnionSerializationSize(s);
-			return size;
-		}
-		default:
-			throw new IllegalStateException("unsupported type " + pvField.getField());
-		}
-	}
-	
 	/**
 	 * @param args
 	 */
@@ -2041,7 +1900,7 @@ public class TestPVDS {
 	    
 	    if (isRx && isTx)
 	    {
-		    long ss = getSerializationSize(data);
+		    long ss = PVDataSerializationSize.getSerializationSize(data);
 	    	byte[] countArray = new byte[1]; byte count = 0;
 		    ByteArrayData bad = new ByteArrayData();
 		    while(true)
@@ -2067,7 +1926,7 @@ public class TestPVDS {
 	    }
 	    else if (isRx)
 	    {
-		    long ss = getSerializationSize(data);
+		    long ss = PVDataSerializationSize.getSerializationSize(data);
 		    ByteArrayData bad = new ByteArrayData();
 		    byte prevv = -1; int missedMessage = 0;
 		    while(true)
